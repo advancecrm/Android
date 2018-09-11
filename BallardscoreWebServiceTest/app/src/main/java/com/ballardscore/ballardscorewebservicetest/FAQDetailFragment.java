4a2670c86e5b7107d4227e16c7ae6c8bac20fe73
advancecrm <42307726+advancecrm@users.noreply.github.com>
2018-09-11 11:05:48 -0600

    Create ballardscore test
    Create ballardscore web service test project with master detail flow.

---
+++ BallardscoreWebServiceTest/app/src/main/java/com/ballardscore/ballardscorewebservicetest/FAQDetailFragment.java
@@ -0,0 +1,69 @@
+package com.ballardscore.ballardscorewebservicetest;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ballardscore.ballardscorewebservicetest.dummy.DummyContent;
+
+/**
+ * A fragment representing a single FAQ detail screen.
+ * This fragment is either contained in a {@link FAQListActivity}
+ * in two-pane mode (on tablets) or a {@link FAQDetailActivity}
+ * on handsets.
+ */
+public class FAQDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public FAQDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.faq_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.faq_detail)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}

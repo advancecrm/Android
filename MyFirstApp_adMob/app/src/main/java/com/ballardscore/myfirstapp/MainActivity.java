90c1d1b5638d47d64a6987d248cd4f2cdf691d2f
advancecrm <42307726+advancecrm@users.noreply.github.com>
2018-09-13 11:15:45 -0600

    MyFirstApp Admob SDK and Init
    AdMob SDK and Init

--- MyFirstApp_adMob/app/src/main/java/com/ballardscore/myfirstapp/MainActivity.java
+++ MyFirstApp_adMob/app/src/main/java/com/ballardscore/myfirstapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -13,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        //ballardscore banner app id
+        MobileAds.initialize(this, "ca-app-pub-7124699817614464~9433246821");
+        //experimental/ sample Id:
+        //MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
     }
 
     /** Called when the user taps the Send button **/

e502ead3dcde53621fb1f7d45d6e7ac0513ccabe
advancecrm <42307726+advancecrm@users.noreply.github.com>
2018-09-13 14:21:47 -0600

    MyFirstApp Interstitial Ad
    Interstitial ad implementation using adClosed event to continue navigation.

--- MyFirstApp_adMob/app/src/main/java/com/ballardscore/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp_adMob/app/src/main/java/com/ballardscore/myfirstapp/DisplayMessageActivity.java
@@ -3,13 +3,18 @@ package com.ballardscore.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    //<!-- AdMob Android Test Interstitial App Id: ca-app-pub-3940256099942544/1033173712  -->
+    private InterstitialAd mInterstitialAd;
     private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +29,36 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView2);
         textView.setText(message);
 
-        //Initialize ads
+        // Show banner add
         MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
         // Load an Ad:
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        //Initialize Interstitial ad
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/1033173712");
+        // Load an Ad:
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+            mInterstitialAd.setAdListener(new AdListener() {
+                @Override
+                public void onAdClosed() {
+                    super.onAdClosed();
+                    finish();
+                }
+            });
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+            //super.onBackPressed();
+        }
     }
 }
